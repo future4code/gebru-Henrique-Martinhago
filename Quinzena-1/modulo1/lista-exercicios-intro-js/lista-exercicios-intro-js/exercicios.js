67373014828a08dd08f6cfe72fdb6b1fccac2ade
Henrique M Martinhago <rick_martignago@hotmail.com>
2021-11-17 23:26:17 -0300

    Aula de string e arrays

---
+++ Quinzena-1/modulo1/lista-exercicios-intro-js/lista-exercicios-intro-js/exercicios.js
@@ -0,0 +1,130 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  let soma = (5 ,10)
+  return 5 + 10
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+ 
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log=(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+  const altura = prompt("diga uma altura")
+  const largura = prompt("diga medida largura")
+  const retangulo = altura*largura;
+  console.log =(retangulo)
+
+}
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+  const anoAtual = Number(prompt("Diga o ano atual"))
+  const nascimento = Number (prompt("Diga seu ano de nascimento"))
+  const idade = anoAtual - nascimento;
+  console.log = (idade)
+
+}
+
+// EXERCÍCIO 03
+/*function calculaIMC(peso, altura) {
+  const pesoKg = "68"
+  const altura = "1,79"
+  return 68 / (1,79*1,79)
+
+} */
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  
+  const nome = prompt ("Qual seu nome?")
+  let idade = Number(prompt ("Qual sua idade?"))
+  let email = prompt ("Qual seu email?")
+
+  console.log = ("Meu nome é",nome,"tenho",idade,"anos,e o meu email é:",email)
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  let cores1 =prompt("Qual sua  cor favorita?")
+  let cores2 =prompt("outra cor favorita?")
+  let cores3 =prompt("Terceira cor favorita?")
+  console.log = (cores1,cores2,cores3)
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  let mensagem ="olá mundo";
+  return UpperCase =mensagem
+
+
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  const checarTamanho = ["bom dia","bom dia"];
+  return "bom dia"==="bom dia"
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+let frutas =["limao","abacaxi","morango"];
+frutas[frutas.length -1];
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  let listaDeNomes = ["joao","maria","paulo","cristiano"];
+  listaDeNomes [listaDeNomes.length-1];
+
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
