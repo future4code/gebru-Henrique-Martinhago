27443564d0aefe114c3ecc41814f25cae22ed69b
Henrique M Martinhago <rick_martignago@hotmail.com>
2022-01-25 19:23:56 -0300

    Alteraçao de exercicio jsx

--- jsx-labetube/jsx-labetube/src/App.js
+++ jsx-labetube/jsx-labetube/src/App.js
@@ -9,10 +9,13 @@ function App() {
   return (
     <div>
       <div className="tela-inteira">
-        <input>
-            <h1>Lab Tube</h1>
-            <input type="text" placeholder="Busca" id="campoDeBusca"></input>
-        </input>
+       
+        <header> 
+        <h1>Lab Tube</h1>
+            <input type="text" placeholder="Busca" id="campoDeBusca"/> 
+        </header>
+           
+            
 
         <main>
             <nav className="menu-vertical">
@@ -28,35 +31,35 @@ function App() {
 
             <section className="painel-de-videos">
                 <div className="box-pagina-principal media1" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=1 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=1 " alt=""/>
                     <h4> {titulo} </h4>
                 </div>
                 <div className="box-pagina-principal media2" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=2 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=2 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media3" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=3 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=3 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media4" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=4 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=4 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media5" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=5 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=5 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media6" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=6 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=6 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media7" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=7 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=7 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media8" onClick={reproduzVideo}>
-                    <img src="https://picsum.photos/400/400?a=8 " alt=""></img>
+                    <img src="https://picsum.photos/400/400?a=8 " alt=""/>
                     <h4>{titulo}</h4>
                 </div>
             </section>
