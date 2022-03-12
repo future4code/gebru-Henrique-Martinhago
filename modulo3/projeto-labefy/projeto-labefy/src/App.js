fb90aed624f512a1d7575b040ac076d2a067f424
Henrique M Martinhago <rick_martignago@hotmail.com>
2022-03-11 22:43:08 -0300

    Labefy em processo

--- modulo3/projeto-labefy/projeto-labefy/src/App.js
+++ modulo3/projeto-labefy/projeto-labefy/src/App.js
@@ -1,50 +1,11 @@
-import axios from 'axios';
-import React from 'react';
+import React from "react";
+import AddPlayList from "./components/AddPlayList";
 
 
-
-class App extends React.Component {
+class App extends React.Component{
   state = {
-    playListInput:"",
+    renderedScreen: 'signup'
   }
 
-  handlePlayListInput = (event) => {
-    this.setState({playListInput:event.target.value})
- }
-
- creatPlayList = async() =>{
-   const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists'
- 
-   const body = {
-     playlist: this.state.playListInput
-   }
-
-   try {
-      const res = await axios.post(url,body, {
-        headers :{
-       Authorization: 'henrique-martignago-gebru'
-        }
-       })
-
-       alert('')
-   }catch (err){
-     
-   }
-  }
   
-
-  render(){
-return (
-<div>
-  <h3>  Tela para Adicionar Playlist</h3>
- <input
- placeholder='Adicione Playlist' ></input>
-
-
-</div>
-);
-  }
 }
- 
-
-export default App;
