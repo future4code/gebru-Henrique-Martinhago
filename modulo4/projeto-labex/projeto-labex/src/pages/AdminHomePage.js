b3ec6a0beab273c93bc9d1baa9ce299bf1ef5cd6
Henrique M Martinhago <rick_martignago@hotmail.com>
2022-03-29 21:23:29 -0300

    Projeto LABEX Iniciado 1 fase

---
+++ modulo4/projeto-labex/projeto-labex/src/pages/AdminHomePage.js
@@ -0,0 +1,24 @@
+import React, {useState,useEffect} from "react";
+import axios from "axios";
+import styled from "styled-components";
+import applicationFormPage from "./ApplicationFormPage"
+import CreatTripPage from "./CreatTripPage"
+import LoginPage from "./LoginPage"
+import ListTripsPage from "./ListTripsPage"
+import TripDetailsPage from "./TripDetailsPage"
+
+const AdmimHomePage =() => {
+
+    return (
+         <div> 
+          <h1> LABEX </h1>
+          <div>
+              <button> </button>
+              <button>  </button>
+          </div>
+         </div> 
+
+    )
+}
+
+export default AdmimHomePage
