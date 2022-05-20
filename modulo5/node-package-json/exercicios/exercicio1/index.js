0a53cccd6a749600d54cc909be156223680feb49
Henrique M Martinhago <rick_martignago@hotmail.com>
2022-05-19 23:12:42 -0300

    exercicio node.js

---
+++ modulo5/node-package-json/exercicios/exercicio1/index.js
@@ -0,0 +1,3 @@
+import {exercicio1} from './exercicio1.js'
+
+console.log (exercicio1)
---
+++ modulo5/node-package-json/exercicios/exercicio2/exercicio2.js
@@ -0,0 +1,24 @@
+// EXERCICIO 2
+export default exercicio2 => {
+    const num1 = 20 
+    const num2 = 5
+    calculos = ()=> {
+        switch(operacao){
+            case "soma":
+                console.log(num1 + num2)
+                break;
+            case "subtrair":
+                console.log(num1 - num2)
+               break
+            case "multiplicacao":
+                console.log(num1 * num2)
+                break
+            case "dividir":
+                console.log(num1 / num2)
+                break    
+     } 
+    } 
+    return exercicio2 
+}
+
+
