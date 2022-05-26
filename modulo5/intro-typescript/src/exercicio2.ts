0738d61a1e5badc7d720352b36a9e60175539f9b
Henrique M Martinhago <rick_martignago@hotmail.com>
2022-05-26 15:19:16 -0300

    finalizado

--- modulo5/intro-typescript/src/exercicio2.ts
+++ modulo5/intro-typescript/src/exercicio2.ts
@@ -2,8 +2,11 @@
 um array que contenha essas três cores. Refatore a função para o Typescript. */
 
 function imprimeTresCoresFavoritas() {
-    const cor1 : string[]= prompt("Insira sua primeira cor favorita")
-    const cor2 : string[]= prompt("Insira sua segunda cor favorita")
-    const cor3 : string[]= prompt("Insira sua terceira cor favorita")
+    const cor1 : string = ("Insira sua primeira cor favorita")
+    const cor2 : string = ("Insira sua segunda cor favorita")
+    const cor3 : string = ("Insira sua terceira cor favorita")
     console.log([cor1, cor2, cor3])
- }
+ }  
+
+ 
+ 
