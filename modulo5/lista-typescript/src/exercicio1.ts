5645234c543a50338be9cdec22bd4900e2b10e58
Henrique M Martinhago <rick_martignago@hotmail.com>
2022-05-30 10:03:58 -0300

    exercicios nao finalizados

--- modulo5/lista-typescript/src/exercicio1.ts
+++ modulo5/lista-typescript/src/exercicio1.ts
@@ -1,27 +1,13 @@
-/*- Exercício 1
-    
-    A seguinte função em JavaScript recebe como parâmetro os tamanhos dos três
-     lados do triângulo: `ladoA, ladoB, ladoC` e retorna se ele é equilátero, isósceles ou escaleno.
-      Refatore a função para o Typescript. */
-    
-    /*function checaTriangulo(a, b, c) {
-      if (a !== b && b !== c) {
-        return "Escaleno";
-      } else if (a === b && b === c) {
-        return "Equilátero";
-      } else {
-        return "Isósceles";
-      }
-    } */
+import { Type } from "typescript"
 
-    function checaTriangulo(a: number, b: number, c:number ):string {
-        if (a !== b && b !== c) {
-          return "Escaleno";
-        } else if (a === b && b === c) {
-          return "Equilátero";
-        } else {
-          return "Isósceles";
-        }
-      }
-      console.log(checaTriangulo(10,10,20))
-    
+
+    const separaData = (nome:string, dataNascimento:string) =>{
+
+       nome ="Henrique"
+       dataNascimento = "26/01/1991"
+       const data : String[] = dataNascimento.split (" / ") 
+       
+       return (`Olá me chamo ${nome},nasci no dia ${data[0]} do mes de ${data[1]} do ano de ${data [3]}`) 
+    }
+  
+    console.log (separaData)
--- modulo5/lista-typescript/src/exercicio2.ts
+++ modulo5/lista-typescript/src/exercicio2.ts
@@ -1,12 +1,7 @@
-/*A seguinte função em JavaScript pergunta ao usuário suas três cores favoritas e imprime no console 
-um array que contenha essas três cores. Refatore a função para o Typescript. */
+//Crie uma função que receba um parâmetro qualquer e que imprima no console o tipo da variável.
 
-function imprimeTresCoresFavoritas() {
-    const cor1 : string = ("Insira sua primeira cor favorita")
-    const cor2 : string = ("Insira sua segunda cor favorita")
-    const cor3 : string = ("Insira sua terceira cor favorita")
-    console.log([cor1, cor2, cor3])
- }  
-
- 
+function tipoVariavel ( tipo:string ) {
+tipo="bom dia "
+}
+ console.log(tipoVariavel) 
  
