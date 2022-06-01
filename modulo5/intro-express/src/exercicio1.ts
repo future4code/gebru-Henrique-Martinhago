7e5b707a9c3785bf1d1032bc7d4340ed8be43ede
Henrique M Martinhago <rick_martignago@hotmail.com>
2022-05-31 22:05:05 -0300

    instalaçoes e exercicio1

---
+++ modulo5/intro-express/src/exercicio1.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import { Request, Response } from "express";
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (resq: Request, res: Response)=>{
+res.status(200).send(" sucesso ")
+})
+
+
+app.listen(3003, () => {
+    console.log(" Servidor funcionando ")
+})
